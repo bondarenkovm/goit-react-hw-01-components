c0a016f50e76ca3b2605631600163a85bde68949
bondarenkovm <bondarenko.vasyl.m@gmail.com>
2023-02-12 23:53:05 +0200

    Styles profile and Statistics

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,25 +2,25 @@ import user from '../user';
 import data from '../data';
 import friends from '../friends';
 import transactions from '../transactions';
-import Profile from './Profile';
-import Statistics from './Statistics';
+import Profile from 'components/Profile/Profile';
+import Statistics from 'components/Statistics/Statistics';
 import FriendList from './FriendList';
 import TransactionHistory from './TransactionHistory';
-// console.log(data);
 
 export const App = () => {
   return (
     <div
       style={{
-        height: '100vh',
+        padding: 20,
         display: 'flex',
+        flexDirection: 'column',
         justifyContent: 'center',
         alignItems: 'center',
         fontSize: 40,
         color: '#010101',
+        backgroundColor: 'rgb(163, 244, 217)',
       }}
     >
-      {/* React homework template */}
       <Profile
         username={user.username}
         tag={user.tag}
