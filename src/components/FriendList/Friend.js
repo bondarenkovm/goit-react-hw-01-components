d3cf610dcbb672ac89e4827239caa53b5222aaea
bondarenkovm <bondarenko.vasyl.m@gmail.com>
2023-02-17 01:24:12 +0200

    status

--- src/components/FriendList/Friend.js
+++ src/components/FriendList/Friend.js
@@ -4,6 +4,7 @@ import css from './Friend.module.css';
 export default function Friend({ avatar, name, status }) {
   return (
     <>
+      {/* <span className={`${css.status} ${css[status]}`}></span> */}
       <span className={status ? css.online : css.offline}></span>
       <img className={css.avatar} src={avatar} alt={name} width="48" />
       <p className={css.name}>{name}</p>
